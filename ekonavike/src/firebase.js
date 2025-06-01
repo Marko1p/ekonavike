a58bf364b065ebce9d78d9991cd3e329d73b1df7
Marko <marko.paladin254@gmail.com>
2025-06-01 02:02:49 +0200

    sitne prepravke

---
+++ ekonavike/src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIRHjDPWTzbhR8aEsA5ZI67TjAaadeupE",
+  authDomain: "ekonavike.firebaseapp.com",
+  projectId: "ekonavike",
+  storageBucket: "ekonavike.firebasestorage.app",
+  messagingSenderId: "419895952992",
+  appId: "1:419895952992:web:dfd7500de44773c845e505",
+  measurementId: "G-H6MRFEWN0V"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth     = getAuth(app)
+export const provider = new GoogleAuthProvider()
+export const db       = getFirestore(app)
